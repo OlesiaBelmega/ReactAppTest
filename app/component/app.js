3898ce52efa20c78b2d88b578df294b446bc0398
Olesia Belmega <olesia.belmega@gmail.com>
2017-06-09 14:50:05 +0300

    irst commit

---
+++ app/component/app.js
@@ -0,0 +1,16 @@
+const React = require('react');
+
+import Popular from './Popular.js';
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <Popular/>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ app/index.js
@@ -0,0 +1,12 @@
+const React = require('react'),
+ReactDOM = require('react-dom'),
+PropTypes = require('prop-types');
+
+import './index.css';
+import App from './component/app.js';
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+);
